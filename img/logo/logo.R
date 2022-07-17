544f28bee90e975b635603b398a2213ad127cafa
Semion Leyn <semen.leyn@gmail.com>
2022-07-17 18:26:46 -0500

    feature: Introduced iJump logo

---
+++ img/logo/logo.R
@@ -0,0 +1,8 @@
+# Built using hexSticker package 
+# (https://github.com/GuangchuangYu/hexSticker)
+library(hexSticker)
+
+logo_img <- "logo_img.png"
+sticker(logo_img, package="iJump", p_size=10, s_x=1, s_y=.75, s_width=.7, p_color = "#000000", h_fill = "#FFFFFF",
+        h_color = "#385492", 
+        filename="logo1.png")
